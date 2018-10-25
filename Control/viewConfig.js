2e7e96b8e3aaa5170cf2b552bae8cd49ac4ec2fb
DESKTOP-L844QFL\iimzo <support@anykeyinteractive.net>
2018-10-25 10:22:24 -0400

    t2

--- Control/viewConfig.js
+++ Control/viewConfig.js
@@ -2,13 +2,13 @@
 mainApp.config(['$routeProvider', function($routeProvider) {
     $routeProvider
       .when('/home', {
-       templateUrl: '../Views/homeView.html', controller: 'MainController'
+       templateUrl: '/Views/homeView.html', controller: 'MainController'
     })
       .when('/login', {
-       templateUrl: '../Views/loginView.html', controller: 'MainController'
+       templateUrl: '/Views/loginView.html', controller: 'MainController'
     })
     .when('/flashcards', {
-      templateUrl: '../Views/flashcardsView.html', controller: 'MainController'
+      templateUrl: '/Views/flashcardsView.html', controller: 'MainController'
     })
       .otherwise({
        redirectTo: '/login'
