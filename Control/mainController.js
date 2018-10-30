f5f010c664bf520870168efc5add192a344c962a
ddimarzio <ddimarzio@powertrain.com>
2018-10-30 15:16:45 -0400

    Reources view 0.14b

--- Control/mainController.js
+++ Control/mainController.js
@@ -2,7 +2,7 @@ var mainApp = angular.module('mainApp', ['ngRoute'] );
 
 mainApp.controller('MainController', function($scope,$location,$window,$localstorage,mainModel) {
  
-  $scope.version = "Version 0.13";
+  $scope.version = "Version 0.14";
 
   $scope.navigateToView = function(viewPage)
   {
