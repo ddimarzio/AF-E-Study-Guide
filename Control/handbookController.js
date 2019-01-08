9841da0f87939a69d2a85214ca55a45e72f06a0d
ddimarzio <ddimarzio@powertrain.com>
2019-01-08 14:40:26 -0500

    Update - 0.130

--- Control/handbookController.js
+++ Control/handbookController.js
@@ -20,7 +20,7 @@ mainApp.controller('HandbookController', function($scope,$sce,$localstorage,$win
                 console.log("response.data : " + JSON.stringify(response.data));
 
                 $scope.allPageContent = [];
-                $scope.allPageContent = response.data.content;
+                $scope.allPageContent = response.data;
 
                 console.log("allPageContent : " + JSON.stringify($scope.allPageContent));
 
