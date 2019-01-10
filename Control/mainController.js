039da60c1cfa83fa4b13623180c0d7421e54f79a
ddimarzio <ddimarzio@powertrain.com>
2019-01-10 12:10:54 -0500

    Update - 0.177

--- Control/mainController.js
+++ Control/mainController.js
@@ -2,7 +2,7 @@ var mainApp = angular.module('mainApp', ['ngRoute','ngAnimate'] );
 
 mainApp.controller('MainController', function($scope,$sce,$location,$window,$localstorage,$document,mainModel,dataService) {
  
-  $scope.version = "Version 0.176";
+  $scope.version = "Version 0.177";
   $scope.Math = window.Math;
 
 
