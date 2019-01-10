088e4d33b4a42206000da6aedaa4d8e6b0272d90
ddimarzio <ddimarzio@powertrain.com>
2019-01-10 12:37:15 -0500

    Update - 0.180

--- Service/mainService.js
+++ Service/mainService.js
@@ -19,7 +19,7 @@ mainApp.service('dataService', ['$http', function ($http,$location) {
 
     this.loginUser = function(thisUser)
     {
-        console.log("host url : " + $location.host());
+        console.log("host url : " + $location.host);
 
         return $http({
             method: 'POST',
