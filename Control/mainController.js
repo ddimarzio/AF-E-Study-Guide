037690b9fffc39d9d89ff4f331365e6c36d00d70
ddimarzio <ddimarzio@powertrain.com>
2019-01-16 14:58:38 -0500

    SD - 0.215

--- Control/mainController.js
+++ Control/mainController.js
@@ -2,7 +2,7 @@ var mainApp = angular.module('mainApp', ['ngRoute','ngAnimate'] );
 
 mainApp.controller('MainController', function($scope,$sce,$location,$window,$localstorage,$document,mainModel,dataService) {
  
-  $scope.version = "Version 0.214";
+  $scope.version = "Version 0.215";
   $scope.Math = window.Math;
 
   // Menu system
