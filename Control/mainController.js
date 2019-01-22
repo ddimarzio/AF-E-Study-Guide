bfb0b494f94c5d370430e5437a72ca6b0538bed7
ddimarzio <ddimarzio@powertrain.com>
2019-01-22 16:14:34 -0500

    Indent - 0.311

--- Control/mainController.js
+++ Control/mainController.js
@@ -2,7 +2,7 @@ var mainApp = angular.module('mainApp', ['ngRoute','ngAnimate'] );
 
 mainApp.controller('MainController', function($scope,$sce,$location,$window,$localstorage,$document,mainModel,dataService) {
  
-  $scope.version = "Version 0.310";
+  $scope.version = "Version 0.311";
 
   $scope.Math = window.Math;
 
