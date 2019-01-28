71300846f6f21a07a90761f7a33e5ec806b56158
ddimarzio <ddimarzio@powertrain.com>
2019-01-28 13:29:49 -0500

    ie11 - 0.474

--- Control/mainController.js
+++ Control/mainController.js
@@ -2,7 +2,7 @@ var mainApp = angular.module('mainApp', ['ngRoute','ngAnimate'] );
 
 mainApp.controller('MainController', function($scope,$sce,$location,$window,$localstorage,$document,mainModel,dataService) {
  
-  $scope.version = "Version 0.473";
+  $scope.version = "Version 0.474";
 
   $scope.Math = window.Math;
 
