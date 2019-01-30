70d5bc164687a43f8acdeddf0ad9d11e83d22c65
ddimarzio <ddimarzio@powertrain.com>
2019-01-30 14:58:23 -0500

    FC - 0.499

--- Control/mainController.js
+++ Control/mainController.js
@@ -2,7 +2,7 @@ var mainApp = angular.module('mainApp', ['ngRoute','ngAnimate'] );
 
 mainApp.controller('MainController', function($scope,$sce,$location,$window,$localstorage,$document,mainModel,dataService) {
  
-  $scope.version = "Version 0.498";
+  $scope.version = "Version 0.499";
 
   $scope.Math = window.Math;
   $(function () {
