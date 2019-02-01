5cf42410b0f50fec0ae8ff90e8950dc016ede674
ddimarzio <ddimarzio@powertrain.com>
2019-02-01 13:12:08 -0500

    Desktop - 0.516

--- Control/mainController.js
+++ Control/mainController.js
@@ -2,7 +2,7 @@ var mainApp = angular.module('mainApp', ['ngRoute','ngAnimate'] );
 
 mainApp.controller('MainController', function($scope,$sce,$location,$window,$localstorage,$document,mainModel,dataService) {
  
-  $scope.version = "Version 0.515";
+  $scope.version = "Version 0.516";
 
   $scope.Math = window.Math;
   $(function () {
